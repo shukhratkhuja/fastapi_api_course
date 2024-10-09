d7274e903acf36a4a8f2948fea4d7901990bfc74
shukhratkhuja <shukhrat.fayzulla@gmail.com>
2024-10-09 17:20:22 +0500

    main.py file cleaned

--- app/database.py
+++ app/database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote
+import psycopg2, time
+from psycopg2.extras import RealDictCursor
+
 
 # SQLALCHEMY_DB_URL = "postgresql://shukhratkhuja:p4stgr2s@localhost/analytic_backup"
 SQLALCHEMY_DB_URL="postgresql://shukhratkhuja:%s@127.0.0.1/analytic_backup" % quote('p4stgr2s') #, echo=True
@@ -19,3 +22,20 @@ def get_db():
         yield db
     except: 
         db.close()
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host='localhost', 
+            database='analytic_backup', 
+            user='shukhratkhuja', 
+            password='p4stgr2s', 
+            cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connection to database failed")
+        print("ERROR: ", str(error))
+        time.sleep(5)
--- app/main.py
+++ app/main.py
@@ -1,15 +1,6 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional, List
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas
-from .database import engine, get_db
-from .utils import hash
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post, user, auth
 
 # from requests import Response
@@ -17,22 +8,6 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine) 
 
 app = FastAPI()
-
-while True:
-    try:
-        conn = psycopg2.connect(
-            host='localhost', 
-            database='analytic_backup', 
-            user='shukhratkhuja', 
-            password='p4stgr2s', 
-            cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successfull!")
-        break
-    except Exception as error:
-        print("Connection to database failed")
-        print("ERROR: ", str(error))
-        time.sleep(5)
 
 
 app.include_router(post.router)
